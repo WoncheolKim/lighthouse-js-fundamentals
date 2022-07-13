8d1c3b72ee51fa425e29670a6b96dc504f5544fb
Woncheol Kim <icijiy@gmail.com>
2022-07-12 17:59:53 -0700

    prep-Declaring and Calling Functions

---
+++ prep-hello.js
@@ -0,0 +1,13 @@
+//const sayhello = function () {
+//  console.log("Hello, world");
+//}
+//
+//sayhello();
+
+const sayHello = function () {
+  console.log("Hello, " + name)
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
