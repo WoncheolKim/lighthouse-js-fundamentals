52f5f7d74220034c429f848941177d095210e2a7
Woncheol Kim <icijiy@gmail.com>
2022-07-16 13:33:38 -0700

    prep-Loopy Lighthouse 2

--- prep-loopylighthouse2.js
+++ prep-loopylighthouse2.js
@@ -1,12 +1,15 @@
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
-for (let i = range[0]; i <= range[1]; i++) {
-  if (i % multiple[0] === 0 && i % multiple[1] === 0) {
+
+let loopyLighthouse = function(range, multiple, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiple[0] === 0 && i % multiple[1] === 0) {
       console.log(i + words[0] + words[1]);
-  } else if (i % multiple[0] === 0) {
+    } else if (i % multiple[0] === 0) {
       console.log(i + words[0]);
-  } else if (i % multiple[1] === 0) {
+    } else if (i % multiple[1] === 0) {
       console.log(i + words[1]);
-  } else {
+    } else {
       console.log(i);
+    }
   }
-}
+};
