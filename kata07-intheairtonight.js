d0c448facbf48f3347cb16dc1790049d8c9d68fc
Woncheol Kim <icijiy@gmail.com>
2022-07-18 11:00:08 -0700

    kata07-In the Air Tonight

--- kata07-intheairtonight.js
+++ kata07-intheairtonight.js
@@ -1,5 +1,24 @@
 const checkAir = function (samples, threshold) {
-  // Code here!
+  let cleanNumber = 0;
+  let dirtyNumber = 0;
+  let result = 0;
+
+  for(let sample of samples) {
+    if(sample === 'clean') {
+      cleanNumber += 1;
+    } else {
+      dirtyNumber += 1;
+    }
+  }
+  let totalNumber = cleanNumber + dirtyNumber;
+
+  result = Math.round(cleanNumber/totalNumber);
+
+  if(result < threshold) {
+    return "Clean";
+  } else {
+    return "Polluted";
+  }
 };
 
 console.log(checkAir(
