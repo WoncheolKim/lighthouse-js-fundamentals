388ebbf73b8986d52b78160595ac2b43c45964b4
Woncheol Kim <icijiy@gmail.com>
2022-07-18 10:56:02 -0700

    kata07-In the Air Tonight

---
+++ kata07-intheairtonight.js
@@ -0,0 +1,18 @@
+const checkAir = function (samples, threshold) {
+  // Code here!
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
