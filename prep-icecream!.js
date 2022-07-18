2ef1b0f0d73ec6a16e46b307e922472aff8f3833
Woncheol Kim <icijiy@gmail.com>
2022-07-18 11:35:01 -0700

    second commit

--- prep-icecream!.js
+++ prep-icecream!.js
@@ -1,12 +1,14 @@
-let iceCreamFlavours = [“chocolate”, “vanilla”, “cookies and cream”, “rocky road”, “strawberry”];
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
 
-// Your code below here…
-iceCreamFlavours.push(“root beer”);
+iceCreamFlavours.push("root beer");
 console.log(iceCreamFlavours);
 
+iceCreamFlavours[0];
 console.log(iceCreamFlavours[0]);
 
-console.log(iceCreamFlavours[-1]);
+iceCreamFlavours[5];
+console.log(iceCreamFlavours[5]);
 
+iceCreamFlavours.length;
 console.log(iceCreamFlavours.length);
