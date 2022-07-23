cecfc941b818cfcad92756fd42625b7c75f90f51
Woncheol Kim <icijiy@gmail.com>
2022-07-22 22:42:26 -0700

    Kata 15 - Organizing Instructors

--- kata15-organizinginstructors.js
+++ kata15-organizinginstructors.js
@@ -1,5 +1,13 @@
 const organizeInstructors = function(instructors) {
-  // Put your solution here
+  const organized = {};
+  for(const i of instructors) {
+    if(organized[i.course] === undefined) {
+      organized[i.course] = [i.name];
+    } else {
+      organized[i.course].push(i.name);
+    }
+  }
+  return organized;
 };
 
 console.log(organizeInstructors([
